f6dbcb74850ca13684c214e5ef87e213e10a2992
tish-code <eliastish2003@gmail.com>
2023-10-20 11:30:28 +0100

    added security with env in firebase file

--- src/components/Auth/firebase-config.ts
+++ src/components/Auth/firebase-config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCMVpOJc57X_2wMeQKmKUTLYrG4nk0EzS8",
-  authDomain: "prdt-be3da.firebaseapp.com",
-  projectId: "prdt-be3da",
-  storageBucket: "prdt-be3da.appspot.com",
-  messagingSenderId: "403395633798",
-  appId: "1:403395633798:web:0c7666c7bb22b06368a707",
-  measurementId: "G-G0DKYFCGL6",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
