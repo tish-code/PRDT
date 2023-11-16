1302553500d35e5f19414639eea432c96d137b48
tish-code <eliastish2003@gmail.com>
2023-11-16 17:01:43 +0100

    fixed auth layout styles

--- src/_components/auth-layout/index.tsx
+++ src/_components/auth-layout/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="flex flex-col items-center h-[100vh] bg-gray-700">
       <div>{children}</div>
     </div>
   );
